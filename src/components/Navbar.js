2dfd299d75f46a923ea501598b26bb8f26537d4d
Rachid Rakaa <rachid1982fsb@gmail.com>
2020-01-10 11:13:02 -0800

    All the main Components in Navbar (Quiz, Profile, Analyze Algorithm)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,23 +8,12 @@ import { NavLink } from 'react-router-dom';
   class Navbar extends React.Component {
     render() {
       return (
-        <div>
-          <NavLink
-            to="/login"
-            exact
-            style={BarLinkStyle.Link}
-            activeStyle={{
-              background: 'darkblue'
-            }}
-          >Login</NavLink>
-            <NavLink
-            to="/about"
-            exact
-            style={BarLinkStyle.Link}
-            activeStyle={{
-              background: 'darkblue'
-            }}
-          >About</NavLink>
+        <div className="ui inverted menu">
+          <NavLink to="/login" exact  className="item"> Login </NavLink>
+          <NavLink to="/quiz" exact  className="item"> Quiz </NavLink>
+          <NavLink to="/profile" exact  className="item"> Profile </NavLink>
+          <NavLink to="/analyzeAlgorithm" exact  className="item"> Analyze Algorithm </NavLink>
+          <NavLink to="/about" exact  className="item"> About </NavLink>
         </div>
       )
     }
