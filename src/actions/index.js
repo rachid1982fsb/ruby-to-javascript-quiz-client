57a2fa94e164d903e523eec699d55441af59528e
Rachid Rakaa <rachid1982fsb@gmail.com>
2020-02-19 15:12:56 -0800

    Change the URL in Logout action to http://ruby-to-javascript-quiz-client.herokuapp.com/

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@
 export const logout =()=>{
     console.log("remove Token")
     localStorage.removeItem('token');
-    window.location.replace("http://127.0.0.1:3000/")
+    window.location.replace("http://ruby-to-javascript-quiz-client.herokuapp.com/")
     return {
         type: "LOGOUT",
         payload: {}
