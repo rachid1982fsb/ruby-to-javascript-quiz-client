95365d1f06df4c11d5848b8852d14efcaf8dcc15
Rachid Rakaa <rachid1982fsb@gmail.com>
2020-02-19 16:45:18 -0800

    Update About page (Add Admin login to access Admin page)

--- src/components/About.js
+++ src/components/About.js
@@ -9,6 +9,9 @@ class About extends React.Component {
               <div class="header">
               About
               </div>
+              <h4> to access admin page sign In as Admin :</h4>
+              <p> username:  Admin </p>
+              <p> password:  admin </p>
               <ul class="list">
                 <li>The user will get a Ruby Method with a name, description, and 2 examples with input & output.</li>
                 <li>Users can translate the ruby method to javascript in an editor container.</li>
